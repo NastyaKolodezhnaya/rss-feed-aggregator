992a80008e01233c172d2c5bda813c0835d407bb
anastasiia <nastya.kolodezhaya@gmail.com>
2026-02-14 14:34:59 +0200

    add new source routes

--- app/routers/sources.py
+++ app/routers/sources.py
@@ -2,22 +2,41 @@ from fastapi import APIRouter
 
 from app.deps import SessionDep
 from app.schemas import AddSourceRequest
-from app.services.source_service import create_source, source_exists
-from app.services.user_service import add_new_user_source, get_user
+from app.services.source_service import add_new_source, delete_source_by_id, source_exists
+from app.services.user_service import add_new_user_source, get_user_by_id
 
 router = APIRouter(prefix='/sources', tags=['sources'])
 
 
 @router.post('/add')
-def add_new_source(*, session: SessionDep, request: AddSourceRequest):
+def add_source(*, session: SessionDep, request: AddSourceRequest):
     # assert user exists
-    user = get_user(session, request.user_id)
+    user = get_user_by_id(session, request.user_id)
     if not user:
         raise ValueError  # todo: raise it properly
 
     existing = source_exists(session, request.feed_link)
     if not existing:
-        existing = create_source(session, request.feed_link)
+        existing = add_new_source(session, request.feed_link)
 
-    add_new_user_source(user, existing)
+    add_new_user_source(session, user, existing)
     return {'result': 'ok'}  # todo: make valid responses
+
+
+@router.get('/{user_id}')
+def get_sources_by_user(*, session: SessionDep, user_id: int):
+    user = get_user_by_id(session, user_id)
+    return {'sources': user.sources}
+
+
+@router.get('/{source_id}')
+def get_source(*, session: SessionDep, source_id: int):
+    return {'source': get_source(session, source_id)}
+    # todo: return SourceNotExistError
+
+
+@router.delete('/{source_id}')
+def delete_source(*, session: SessionDep, source_id: int):
+    delete_source_by_id(session, source_id)
+    return {'result': 'ok'}
+    # todo: return SourceNotExistError
--- app/services/source_service.py
+++ app/services/source_service.py
@@ -1,19 +1,19 @@
 from dataclasses import asdict
 
-from sqlalchemy import select
+from sqlalchemy import delete, select
 
 from app.models import Entry as Entry_Db
 from app.models import Source as Source_Db
 from app.utils.parse import construct_new_feed
 
 
-def source_exists(session, feed_link: str):
+def source_exists(session, feed_link: str) -> Source_Db | None:
     stmt = select(Source_Db).where(Source_Db.feed_link == feed_link)
     result = session.execute(stmt)
     return result.scalars().first() or None
 
 
-def create_source(session, feed_link: str):
+def add_new_source(session, feed_link: str) -> Source_Db:
     source_data = construct_new_feed(feed_link)
 
     source_kwargs = asdict(source_data)
@@ -23,3 +23,15 @@ def create_source(session, feed_link: str):
 
     session.add(source_db)
     session.commit()
+    return source_db
+
+
+def get_source_by_id(session, source_id: int) -> Source_Db:
+    stmt = select(Source_Db).where(Source_Db.id == source_id)
+    result = session.execute(stmt)
+    return result.scalars().first() or None
+
+
+def delete_source_by_id(session, source_id: int):
+    stmt = delete(Source_Db).where(Source_Db.id == source_id)
+    session.execute(stmt)
